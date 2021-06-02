c55828c1d40ff87e84f44174b6eec0f4a5f5e2a7
Karimzhon Masaitov <kmasaitov1988@gmail.com>
2021-06-01 23:11:43 -0500

    commit 2

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from main");
+
+        System.out.println("developing hello feature");
     }
 }
